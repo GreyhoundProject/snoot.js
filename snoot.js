6ef58902001e8a2656c3813e9bbb519c6b462a96
James <72380604+GreyhoundProject@users.noreply.github.com>
2020-11-14 15:53:55 +0000

    Tween Class Implemented

--- snoot.js
+++ snoot.js
@@ -1,9 +1,15 @@
+/**
+ * Sets a group of key/value style properties to a given element.
+ * @param {object} element - HTML Element.
+ * @param {object} properties - Key value pairs of style properties.
+ * @returns {boolean} Returns false if element is not valid
+ */
 function css(element, properties)
 {
-    if (element === 'object' && element !== null)
+    if (typeof element === 'object' && element !== null)
         return false;
 
-    if (properties === 'object' && proporties !== null)
+    if (typeof properties === 'object' && properties !== null)
         return false;
 
     for (const p in properties)
@@ -14,12 +20,19 @@ function css(element, properties)
     return true;
 }
 
+
+/**
+ * Sets a group of key/value style properties to a given element.
+ * @param {object} element - HTML Element.
+ * @param {object} attributes - Key value pairs of attributes.
+ * @returns {boolean} Returns false if element is not valid
+ */
 function att(element, attributes)
 {
-    if (element === 'object' && element !== null)
+    if (typeof element === 'object' && element !== null)
         return false;
 
-    if (attributes === 'object' && attributes !== null)
+    if (typeof attributes === 'object' && attributes !== null)
         return false;
 
     for (const a in attributes)
@@ -36,42 +49,77 @@ function clamp(value, min, max)
     return value;
 }
 
-function linInterp(value, min, max)
-{
-    let t = (max-min)/100;
-    return t*value;
-}
 
-function sinInterp(value, min, max)
+/*
+ *
+ *  TWEEN CLASS
+ *
+ */ 
+class Tween
 {
-    let v = clamp(value, min, max);
-    let mx = max-min;
-    let hpi = Math.PI/2;
-    let t = Math.sin( ( ((v-min)/mx) *Math.PI)-hpi );
-    return (t/2)+0.5;
-}
+    /** Tween Class: Use setRange(min,max) to set range, and calculate
+     *  interpolation using lin(t), sin(t), exp(t), where 't' is a
+     *  number between 0-1.
+     */
+    constructor()
+    {
+        this.min = 0;
+        this.max = 1;
+        this.dR = 1;
+    }
 
-function quadInterp(value, min, max)
-{
-    return powInterp(value, mim, max, 2);
-}
+    /**
+     * Sets range bounds, and calculated range difference (dR).
+     * @param {number} min - Lower range limit.
+     * @param {} max - Upper range limit.
+     * @returns {null}
+     */
+    setRange(min, max)
+    {
+        this.min = min;
+        this.max = max;
+        this.dR = max-min;
+    }
 
-function expInterp(value, min, max, exp)
-{
-    let v = clamp(value, min, max);
-    let mx = max-min;
-    let t = Math.pow((v-min)/mx, exp>0? exp:1);
-    return t;
-}
+    /**
+     * Returns the linear interpolation between min and max
+     * given by parameter 't'.
+     * @param {number} t - Decimal number between 0-1.
+     * @returns {number} Linear interpolation between min-max (default 0-1).
+     */
+    lin(t)
+    {
+        let T = clamp(t, 0, 1);
+        return ((this.dR*t) + this.min);
+    }
 
-function quadInterp(value, min, max)
-{
-    return expInterp(value, min, max, 2);
-}
+    /**
+     * Returns the sinusoidal interpolation between min and max
+     * given by parameter 't'. Mapped between -PI/2 to PI/2.
+     * @param {number} t - Decimal number between 0-1.
+     * @returns {number} Sinusoidal interpolation between min-max (default 0-1).
+     */
+    sin(t)
+    {
+        let T = clamp(t, 0, 1);
+        let U = Math.sin( ( T*Math.PI - (Math.PI/2) ) );
+        return (this.dR * ((U/2)+0.5) + this.min);
+    }
 
-function sqrtInterp(value, min, max)
-{
-    return expInterp(value, min, max, 0.5);
+    /**
+     * Returns the exponential interpolation between min and max
+     * given by parameter 't' and exponent 'E'.
+     * @param {number} t - Decimal number between 0-1.
+     * @param {number} E - Exponential value (x^E).
+     * @returns {number} Exponential interpolation between min-max (default 0-1).
+     */
+    exp(t, E=2)
+    {
+        let T = clamp(t, 0, 1);
+        return (this.dR * Math.pow(T, E)) + this.min;
+    }
+
+
 }
 
 
@@ -87,19 +135,35 @@ let svg = document.getElementById('svgc');
 
 css(svg, {'borderWidth': '2px', 'borderColor': 'black', 'borderStyle':'solid'});
 
-for (let c=0; c<=20; c++)
+var linLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+        att(linLine, {
+            'x1': 0,
+            'y1': 1,
+            'x2': 1,
+            'y2': 0,
+            'stroke': '#CCCCCC',
+            'stroke-width':'0.005'
+        });
+        svg.appendChild(linLine);
+
+
+let t = new Tween();
+t.setRange(0.25,0.75);
+
+for (let c=0; c<=100; c++)
 {
-    let ny = (200 - (200*sqrtInterp(c, 0, 20)));
+    let ny = (1 - (t.sin(c/100)));
     if (c != 0)
     {
         var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
 
         att(newLine, {
-            'x1': (c-1)*10,
+            'x1': (c-1)/100,
             'y1': y,
-            'x2': c*10,
+            'x2': c/100,
             'y2': ny,
-            "stroke": "black",
+            'stroke': "black",
+            'stroke-width':'0.005'
         });
         svg.appendChild(newLine);
     }
